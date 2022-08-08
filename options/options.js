b331993bacd83a48f03a347d22a9149173da165c
Crowdbotics <team@crowdbotics.com>
2022-08-08 15:25:08 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "tortas_paquime_cate_36667",
+  url: "https://tortas_paquime_cate_36667.botics.co",
+  api: "https://tortas_paquime_cate_36667.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
